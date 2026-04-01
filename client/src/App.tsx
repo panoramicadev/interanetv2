ad8e237a07ef991c23881088c375eb236d8ec7d7
primerosresultados <primerosresultados@gmail.com>
2026-04-01 13:44:13 -0300

    fix(ui): restore missing Toaster component to show validation errors globally

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
 import { CartProvider } from "@/contexts/CartContext";
@@ -373,6 +374,7 @@ function App() {
       <FilterProvider>
         <CartProvider>
           <TooltipProvider>
+            <Toaster />
             <Router />
             <UpdateNotification />
           </TooltipProvider>
